10fb910b950128ff78f68c9770368db254ab9c7a
mrebollo <mrebollob@gmail.com>
2015-11-21 16:36:55 +0100

    Added base project

---
+++ app/src/androidTest/java/com/mrebollob/loteriadenavidad/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mrebollob.loteriadenavidad;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
