f725db0811328034232fb77993c39aca2eb77e16
mrebollob <mrebollob@gmail.com>
2015-12-14 19:09:02 +0100

    Added crashlytics

--- app/src/main/java/com/mrebollob/loteriadenavidad/app/LoteriaDeNavidadApp.java
+++ app/src/main/java/com/mrebollob/loteriadenavidad/app/LoteriaDeNavidadApp.java
@@ -3,9 +3,11 @@ package com.mrebollob.loteriadenavidad.app;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.mrebollob.loteriadenavidad.app.di.AppModule;
 
 import dagger.ObjectGraph;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * @author mrebollob
@@ -16,6 +18,7 @@ public class LoteriaDeNavidadApp extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         initObjectGraph();
     }
 
